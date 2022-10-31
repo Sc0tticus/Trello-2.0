28684c76fd54dbbdefc6ed32a226b8c5a700d0af
Sc0tticus <ssinger303@gmail.com>
2022-10-30 21:41:56 -0600

    Matching project to Trello-Clone. This project has a index.d.ts file!

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
-export const App = () => {
-  return null
-}
+import { AppContainer } from "./styles" //
+import { Column } from './Column' //
+import { AddNewItem } from './AddNewItem' //
+import { useAppState } from './state/AppStateContext' //
 
-export default App;
+export const App = () => { //
+	const { lists } = useAppState() //
+
+	return (
+		<AppContainer>
+			{lists.map(list => (
+					<Column text={list.text} key={list.id} id={list.id} />
+			))}
+			<AddNewItem
+				toggleButtonText='+ Add another list'
+				onAdd={() => console.log('Item created')}
+			/>
+		</AppContainer>
+	)
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
+import {
+	AppStateProvider,
+	AppStateContext
+} from './state/AppStateContext';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
 root.render(
-  <React.StrictMode>
-    <App />
+	<React.StrictMode>
+		<AppStateProvider>
+			<App />
+		</AppStateProvider>
   </React.StrictMode>
 );
 
+export const useAppState = () => {
+return useContext(AppStateContext)
+}
